034ab5e7f70b14edef95514a91c711b929d3b1c6
DESKTOP-6JDTKNG\alex_ <alex_gordo50@hotmail.com>
2018-01-28 14:35:10 +0000

    Logo adde

---
+++ GGJ/Assets/Logo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logo : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject logo;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+          
+            mainMenu.SetActive(true);
+            logo.SetActive(false);
+        }
+    }
+}
--- GGJ/Assets/Scripts/MainMenu.cs
+++ GGJ/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame()
+ 
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+    
+
+        
+    }
+
+    public void PlayGame()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
